a6ada9a9c96cfcaefed11b5ed61c5599963bf6b3
Patrick McQuery <patrickmcquery@gmail.com>
2022-11-05 23:39:55 -0700

    Added "This is a test!" to test my github auth

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.print("Hello Club!");
+        System.out.println("Hello Club!");
+        System.out.println("This is a test!");
     }
 }
